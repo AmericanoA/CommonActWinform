ff90c8d422e46750bb6d612925b5a9854ae00964
selamela <109020825+selamela@users.noreply.github.com>
2023-01-09 21:39:00 +0900

    IEnumerator 형식 자유

--- CommonActWinform/Interface/IEnumerableFolder/BookEnum.cs
+++ CommonActWinform/Interface/IEnumerableFolder/BookEnum.cs
@@ -17,13 +17,25 @@ namespace CommonActWinform.Interface.IEnumerableFolder
             {
                 1,2,3,4,5,6,7,8,9,10
             };
-
+            private string[] data2 = new string[]
+            {
+                "A","B","C","D","E","F","G","H","I"
+            };
             private int pos = 0;
             public object Current
             {
+                //+ Current 안에서 pos 위치에따라 반환값 자유 변경
                 get
                 {
-                    return data[pos - 1];
+                    if (pos < data.Length)
+                    {
+
+                        return data[pos - 1];
+                    }
+                    else
+                    {
+                        return data2[pos - 10];
+                    }
                 }
             }
             public bool MoveNext()
@@ -32,7 +44,10 @@ namespace CommonActWinform.Interface.IEnumerableFolder
                 if (pos >= data.Length)
                 {
                     //4. false 반환시 foreach 종료
-                    return false;
+                    if (pos >= data2.Length + 9)
+                    {
+                        return false;
+                    }
                 }
                 pos++;
 
