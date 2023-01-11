6b72e67881ddde5ca6e9fd84890eaba53305b0a6
selamela <109020825+selamela@users.noreply.github.com>
2023-01-11 21:43:12 +0900

    최선

--- CommonActWinform/MainForm.Designer.cs
+++ CommonActWinform/MainForm.Designer.cs
@@ -31,6 +31,9 @@ namespace CommonActWinform
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -53,15 +56,55 @@ namespace CommonActWinform
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnF2;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(695, 426);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
+            this.dataGridView1.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dataGridView1_CellFormatting);
+            this.dataGridView1.CellLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellLeave);
+            this.dataGridView1.CellPainting += new System.Windows.Forms.DataGridViewCellPaintingEventHandler(this.dataGridView1_CellPainting);
+            this.dataGridView1.ColumnHeaderMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_ColumnHeaderMouseDoubleClick);
+            this.dataGridView1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.dataGridView1_PreviewKeyDown);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(713, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // CommonAct
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "CommonAct";
             this.Text = "CommonAct";
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.CommonAct_PreviewKeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -70,6 +113,8 @@ namespace CommonActWinform
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- CommonActWinform/MainForm.cs
+++ CommonActWinform/MainForm.cs
@@ -13,6 +13,9 @@ namespace CommonActWinform
 {
     public partial class CommonAct : Form
     {
+        private int selcol;
+
+        private bool bselheader = false;
         public CommonAct()
         {
             InitializeComponent();
@@ -41,7 +44,110 @@ namespace CommonActWinform
                 Console.WriteLine(item.ToString());
             }
 
-            
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+
+            for (int i = 0; i < 10; i++)
+            {
+                dt.Columns.Add(i.ToString());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                dt.Rows.Add(i);
+            }
+
+            dataGridView1.DataSource = dt;
+
+            foreach (DataGridViewColumn item in dataGridView1.Columns)
+            {
+                item.SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+            dataGridView1.Rows[0].Height = 0;
+        }
+
+        private void dataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            Console.WriteLine($"Col : {e.ColumnIndex}, Row: {e.RowIndex} -  Formattion");
+        }
+
+        private void dataGridView1_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            Console.WriteLine($"Col : {e.ColumnIndex}, Row: {e.RowIndex} -  Painting");
+        }
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            Console.WriteLine($"Col : {e.ColumnIndex}, Row: {e.RowIndex} -  CellEnter");
+        }
+
+        private void dataGridView1_CellLeave(object sender, DataGridViewCellEventArgs e)
+        {
+            Console.WriteLine($"Col : {e.ColumnIndex}, Row: {e.RowIndex} -  CellLeave");
+        }
+
+        private void dataGridView1_ColumnHeaderMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            Console.WriteLine($"Col : {e.ColumnIndex}, Row: {e.RowIndex} -  Header Double Click");
+
+            if (bselheader)
+            {
+                ColumnSelect(selcol,Color.White);
+            }
+            selcol = e.ColumnIndex;
+            ColumnSelect(selcol, Color.Black);
+            bselheader = true;
+
+            dataGridView1.ClearSelection();
+        }
+
+        private void CommonAct_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (bselheader)
+            {
+                if (e.KeyCode == Keys.Up || e.KeyCode == Keys.Down || e.KeyCode == Keys.Left || e.KeyCode == Keys.Right)
+                {
+                    if (e.KeyCode == Keys.Right)
+                    {
+                        ColumnSelect(selcol, Color.White);
+                        selcol += 1;
+                        ColumnSelect(selcol,Color.Black);
+                    }
+                    else if (e.KeyCode == Keys.Left)
+                    {
+                        ColumnSelect(selcol, Color.White);
+                        selcol -= 1;
+                        ColumnSelect(selcol, Color.Black);
+                    }
+                    else if (e.KeyCode == Keys.Down)
+                    {
+                        ColumnSelect(selcol, Color.White);
+                        bselheader = false;
+                    }
+                    dataGridView1.CurrentCell = dataGridView1.Rows[0].Cells[selcol];
+                }
+            }
+        }
+        private void ColumnSelect(int colindex, Color color)
+        {
+            this.Invoke(new Action(() =>
+            {
+                dataGridView1.EnableHeadersVisualStyles = false;
+                dataGridView1.Columns[selcol].HeaderCell.Style.BackColor = color;
+
+                for (int i = 0; i < dataGridView1.RowCount; i++)
+                {
+                    dataGridView1[colindex, i].Style.BackColor = color;
+                }
+            }));
         }
     }
 
